c2d1740a12c639fc4130e725c19fa9486dbe767c
Ryan1678 <ryanamorim623@gmail.com>
2024-08-08 19:16:24 -0300

    PizzariaItb

---
+++ src/shared/Pizzas.jsx
@@ -0,0 +1,61 @@
+import Mussarela from "../assets/image/Mussarela.png"
+import Calabresa from "../assets/image/Calabresa.png"
+import Atum from "../assets/image/Atum.png"
+import Portuguesa from "../assets/image/Portuguesa.png"
+import Frango from "../assets/image/Frango.png"
+
+const Pizzas = [
+    {
+        "id": 1,
+        "nome": "Mussarela",
+        "descricao": "Molho de tomate e mussarela.",
+        "imagem": Mussarela
+    },
+    {
+        "id": 2,
+        "nome": "Frango com Catupiry",
+        "descricao": "Molho de tomate, mussarela, frango desfiado e catupiry",
+        "imagem": Frango
+    },
+    {
+        "id": 3,
+        "nome": "Portuguesa",
+        "descricao": "Molho de tomate, mussarela, presunto, ovo, cebola, azeitona e orégano.",
+        "imagem": Portuguesa
+    },
+    {
+        "id": 4,
+        "nome": "Calabresa",
+        "descricao": "Molho de tomate, mussarela, calabresa e cebola.",
+        "imagem": Calabresa
+    },
+    {
+        "id": 5,
+        "nome": "Atum",
+        "descricao": "Molho de tomate, mussarela, atum e tomate",
+        "imagem": Atum
+    },
+]
+
+export default Pizzas;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
